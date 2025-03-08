b6df54c1566191024f9fd264fbfd776829a21685
ducbinhnguyennd <ducbinhnguyennd@gmail.com>
2025-03-09 00:30:48 +0700

    update day 4

--- src/Layout/TrangChuLayout/MatchDetails/MatchDetails.js
+++ src/Layout/TrangChuLayout/MatchDetails/MatchDetails.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const MatchDetails = () => {
   const [showModal, setShowModal] = useState(false);
+  const [betAmount, setBetAmount] = useState(""); // State để lưu số tiền chọn trong modal
 
   return (
     <div className="container-detailsmatch">
@@ -18,25 +19,29 @@ const MatchDetails = () => {
         </div>
       </header>
 
-     
-        <div className="game-info1">
-          <div className="league-name">
-            <h4>Cambodian Premier League</h4>
-          </div>
-          <div className="team-box">
-            <Team name="Angkor Tiger FC" logo="/assets/images/doi1.jpe" />
-            <GameDate date="23:13 28/12" countdown="01:00:00" />
-            <Team name="Angkor Tiger FC" logo="/assets/images/doi1.jpe" />
-          </div>
+      <div className="game-info1">
+        <div className="league-name">
+          <h4>Cambodian Premier League</h4>
         </div>
-
-        <div className="divnghichtyso">
-          <Tabs />
-          <ScorePredictions openModal={() => setShowModal(true)} />
+        <div className="team-box">
+          <Team name="Angkor Tiger FC" logo="/assets/images/doi1.jpe" />
+          <GameDate date="23:13 28/12" countdown="01:00:00" />
+          <Team name="Angkor Tiger FC" logo="/assets/images/doi1.jpe" />
         </div>
-      
+      </div>
 
-      {showModal && <BetModal closeModal={() => setShowModal(false)} />}
+      <div className="divnghichtyso">
+        <Tabs />
+        <ScorePredictions openModal={() => setShowModal(true)} />
+      </div>
+
+      {showModal && (
+        <BetModal
+          closeModal={() => setShowModal(false)}
+          betAmount={betAmount}
+          setBetAmount={setBetAmount} // Truyền state xuống để cập nhật tiền
+        />
+      )}
     </div>
   );
 };
@@ -102,25 +107,75 @@ const ScorePredictions = ({ openModal }) => {
   );
 };
 
-const BetModal = ({ closeModal }) => (
-  <div id="modal" className="modal">
-    <div className="modal-content">
-      <div className="tieudemodal">
-        <button className="close-modal" onClick={closeModal}>x</button>
-        <h4>Đặt cược</h4>
+const BetModal = ({ closeModal, betAmount, setBetAmount }) => {
+  const handleAmountClick = (value) => {
+    setBetAmount(value); // Cập nhật số tiền vào state
+  };
+
+  return (
+    <div id="modal" className="modal-detailmatch">
+      <div className="modal-content-detailmatch">
+        <div className="tieudemodal">
+          <button className="close-modal" onClick={closeModal}>x</button>
+          <h4>Đặt cược</h4>
+        </div>
+        <div className="divtysomodal">
+          <p>FT Tỷ số</p>
+          <p>0:0</p>
+          <p>@2.22%</p>
+        </div>
+        <div className="teamtime">
+          <img src="/assets/images/doi1.jpe" alt="Team 1" />
+          <div className="ngaygio">
+            <p>18:00</p>
+            <p>27/12/2024</p>
+            <p>01:36:47</p>
+          </div>
+          <img src="/assets/images/doi2.webp" alt="Team 2" />
+        </div>
+        <form>
+          <div className="sotien">
+            <p>Số tiền:</p>
+            <input
+              placeholder="Hạn mức một giao dịch tối thiểu 10 coin"
+              id="input-sotien"
+              value={betAmount} // Hiển thị giá trị đã chọn
+              readOnly // Không cho phép nhập tay
+              required
+            />
+          </div>
+          <div className="options">
+            <button type="button" onClick={() => handleAmountClick("200")}>200</button>
+            <button type="button" onClick={() => handleAmountClick("500")}>500</button>
+            <button type="button" onClick={() => handleAmountClick("1000")}>1,000</button>
+            <button type="button" onClick={() => handleAmountClick("2000")}>2,000</button>
+            <button type="button" onClick={() => handleAmountClick("5000")}>5,000</button>
+            <button type="button" onClick={() => handleAmountClick("Toàn bộ")}>Toàn bộ</button>
+          </div>
+          <div className="divtyle">
+            <div className="tieudetyle">
+              <p>Tỷ lệ thắng</p>
+              <p>Lợi nhuận kì vọng</p>
+              <p>Phí thủ tục</p>
+              <p>Lợi nhuận ròng</p>
+            </div>
+            <div className="tyle">
+              <p>x2.22%</p>
+              <p>0</p>
+              <p>0</p>
+              <p>0</p>
+            </div>
+          </div>
+          <div className="btnmodal">
+            <button className="btnhuy" type="button" onClick={closeModal}>
+              Hủy bỏ
+            </button>
+            <button type="submit" className="place-bet">Đặt cược</button>
+          </div>
+        </form>
       </div>
-      <form>
-        <div className="sotien">
-          <p>Số tiền:</p>
-          <input placeholder="Hạn mức một giao dịch tối thiểu 10 coin" disabled required />
-        </div>
-        <div className="btnmodal">
-          <button className="btnhuy" type="button" onClick={closeModal}>Hủy bỏ</button>
-          <button type="submit" className="place-bet">Đặt cược</button>
-        </div>
-      </form>
     </div>
-  </div>
-);
+  );
+};
 
 export default MatchDetails;
